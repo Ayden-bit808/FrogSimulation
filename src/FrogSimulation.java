4e2181b3b129992a5e249c2e2ef925b4ec61bdc6
AydenArenas <aydena19@nycstudents.net>
2025-11-20 11:49:52 -0500

    Initial commit

---
+++ src/FrogSimulation.java
@@ -0,0 +1,33 @@
+public class FrogSimulation {
+    private int goalDistance;
+    private int maxHops;
+
+    public FrogSimulation(int dist, int numHops)
+    {
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+
+    private int hopDistance()
+    {
+      return (int) (Math.random() * 40) - 8;
+    }
+
+    public boolean simulate(){
+        int distance = 0;
+        while (maxHops > 0) {
+            distance += hopDistance();
+            System.out.println(distance);
+            if (distance >= goalDistance)
+                return true;
+            if (distance < 0) return false;
+            maxHops--;
+        }
+        return false;
+
+    }
+
+    public double runSimulations(int num){
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+       FrogSimulation fs = new FrogSimulation(24, 5);
+        System.out.println(fs.simulate());
+
+    }
+}
+
